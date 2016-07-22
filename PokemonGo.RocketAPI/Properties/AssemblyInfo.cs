30caf777e7336f07cef3fe940328bfb1ed8165be
NecronomiconCoding <tvgfx123@gmail.com>
2016-07-22 21:47:11 +0200

    improved evolve brainfart :D

--- PokemonGo.RocketAPI/Properties/AssemblyInfo.cs
+++ PokemonGo.RocketAPI/Properties/AssemblyInfo.cs
@@ -39,5 +39,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.1.4")]
+[assembly: AssemblyVersion("1.0.1.5")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
