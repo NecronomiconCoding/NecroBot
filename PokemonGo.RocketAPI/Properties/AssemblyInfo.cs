4328ce7ee8d929d623d338a98c50a19977667595
NecronomiconCoding <tvgfx123@gmail.com>
2016-07-23 14:36:26 +0200

    now catches not verified accounts

--- PokemonGo.RocketAPI/Properties/AssemblyInfo.cs
+++ PokemonGo.RocketAPI/Properties/AssemblyInfo.cs
@@ -39,5 +39,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.3.3")]
+[assembly: AssemblyVersion("1.0.3.4")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
