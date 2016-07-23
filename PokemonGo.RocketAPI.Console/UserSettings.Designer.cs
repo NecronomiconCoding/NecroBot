8d2b3daa40035bd9893097aa797203bc6d0fc9ec
PocketMobsters <pitime2@outlook.com>
2016-07-23 13:43:12 -0500

    Add PrioritizeIVOverCP setting

--- PokemonGo.RocketAPI.Console/UserSettings.Designer.cs
+++ PokemonGo.RocketAPI.Console/UserSettings.Designer.cs
@@ -202,5 +202,17 @@ namespace PokemonGo.RocketAPI.Console {
                 this["KeepMinDuplicatePokemon"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool PrioritizeIVOverCP {
+            get {
+                return ((bool)(this["PrioritizeIVOverCP"]));
+            }
+            set {
+                this["PrioritizeIVOverCP"] = value;
+            }
+        }
     }
 }
