dc00250bde7a80759e2fbdbf3d5fd5d4ff552503
Andrew Blakely <andrew.blakely@ymail.com>
2016-07-24 15:47:37 -0500

    Added Multiple Response Payload Handling

--- PokemonGo.RocketAPI/Extensions/HttpClientExtensions.cs
+++ PokemonGo.RocketAPI/Extensions/HttpClientExtensions.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Google.Protobuf;
 using PokemonGo.RocketAPI.Exceptions;
 using PokemonGo.RocketAPI.GeneratedCode;
+using System.Linq;
+using System.Collections.Generic;
 
 #endregion
 
@@ -39,12 +41,31 @@ namespace PokemonGo.RocketAPI.Extensions
                 throw new InvalidResponseException();
 
             //Decode payload
-            //todo: multi-payload support
             var payload = response.Payload[0];
             var parsedPayload = new TResponsePayload();
             parsedPayload.MergeFrom(payload);
 
             return parsedPayload;
+        }
+
+        public static async Task<IEnumerable<TResponsePayload>> PostProtoPayloadExpectMultiple<TRequest, TResponsePayload>(this HttpClient client,
+            string url, TRequest request) where TRequest : IMessage<TRequest>
+            where TResponsePayload : IMessage<TResponsePayload>, new()
+        {
+            Logger.Write($"Requesting multiple {typeof(TResponsePayload).Name}", LogLevel.Debug);
+            var response = await PostProto(client, url, request);
+
+            if (response.Payload.Count == 0)
+                throw new InvalidResponseException();
+
+            //Decode payload
+            return response.Payload.Select(sb =>
+            {
+                TResponsePayload responsePayload = new TResponsePayload(); //this is slowish. Compiler emits IL for Activator.CreateNew on generic new() constraint. Consider compiled lambda in the future.
+                responsePayload.MergeFrom(sb);
+
+                return responsePayload;
+            });
         }
     }
 }
