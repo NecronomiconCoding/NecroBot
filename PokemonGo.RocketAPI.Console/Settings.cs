a373d5362a0dc63504af51751f7997ab14f6a9cb
pattythesleeper <littekreativ@gmail.com>
2016-07-24 08:17:40 +0300

    Small fixes for PokemonsNotToCatch
    Made a very small fix on PokemonsNotToCatch. Now it wont by default catch 
    
    Zubat
    Pidgey
    Rattata

--- PokemonGo.RocketAPI.Console/Settings.cs
+++ PokemonGo.RocketAPI.Console/Settings.cs
@@ -74,7 +74,7 @@ namespace PokemonGo.RocketAPI.Console
             get
             {
                 //Type of pokemons to evolve
-                var defaultText = new string[] { "Zubat", "Pidgey", "Ratata" };
+                var defaultText = new string[] { "Zubat", "Pidgey", "Rattata" };
                 _pokemonsToEvolve = _pokemonsToEvolve ?? LoadPokemonList("Configs\\ConfigPokemonsToEvolve.txt", defaultText);
                 return _pokemonsToEvolve;
             }
@@ -97,7 +97,7 @@ namespace PokemonGo.RocketAPI.Console
             get
             {
                 //Type of pokemons not to catch
-                var defaultText = new string[] { "Zubat", "Pidgey", "Ratata" };
+                var defaultText = new string[] { "Zubat", "Pidgey", "Rattata" };
                 _pokemonsNotToCatch = _pokemonsNotToCatch ?? LoadPokemonList("Configs\\ConfigPokemonsNotToCatch.txt", defaultText);
                 return _pokemonsNotToCatch;
             }
