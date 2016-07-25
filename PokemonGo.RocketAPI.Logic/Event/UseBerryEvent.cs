6329f8180c37870b313661272e5d5bc6ab44daf3
PocketMobsters <pitime2@outlook.com>
2016-07-25 12:47:00 -0500

    Added UseBerryEvent.cs

---
+++ PokemonGo.RocketAPI.Logic/Event/UseBerryEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonGo.RocketAPI.Logic.Event
+{
+    public class UseBerryEvent : IEvent
+    {
+        public int Count;
+    }
+}
