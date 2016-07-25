9ecc135482467bc46055428bca663b42303f71e3
Maxime GRIOT <yamashi@live.com>
2016-07-25 14:56:46 +0200

    Removed old stuff

--- PokemonGo.RocketAPI.Console/Program.cs
+++ PokemonGo.RocketAPI.Console/Program.cs
@@ -31,31 +31,6 @@ namespace PokemonGo.RocketAPI.Console
             machine.SetFailureState(new LoginState());
             machine.AsyncStart(new VersionCheckState(), new Context(new Settings()));
 
-            /*Task.Run(() =>
-            {
-                try
-                {
-                    new Logic.Logic(new Settings()).Execute().Wait();
-                }
-                catch (PtcOfflineException)
-                {
-                    Logger.Write("PTC Servers are probably down OR your credentials are wrong. Try google",
-                        LogLevel.Error);
-                    Logger.Write("Trying again in 20 seconds...");
-                    Thread.Sleep(20000);
-                    new Logic.Logic(new Settings()).Execute().Wait();
-                }
-                catch (AccountNotVerifiedException)
-                {
-                    Logger.Write("Account not verified. - Exiting");
-                    Environment.Exit(0);
-                }
-                catch (Exception ex)
-                {
-                    Logger.Write($"Unhandled exception: {ex}", LogLevel.Error);
-                    new Logic.Logic(new Settings()).Execute().Wait();
-                }
-            });*/
             System.Console.ReadLine();
         }
     }
