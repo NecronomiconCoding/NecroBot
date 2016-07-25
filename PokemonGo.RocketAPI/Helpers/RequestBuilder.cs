98417d5cde86be58caabcf0291f07f52c62246ff
Andrew Blakely <andrew.blakely@ymail.com>
2016-07-24 21:08:04 -0500

    Replaced GetRequest with GetRequestEnvelope

--- PokemonGo.RocketAPI/Helpers/RequestBuilder.cs
+++ PokemonGo.RocketAPI/Helpers/RequestBuilder.cs
@@ -22,7 +22,8 @@ namespace PokemonGo.RocketAPI.Helpers
 			envelope.WithAltitude(altitude)
 				.WithLatitude(lat)
 				.WithLongitude(lng)
-				.WithAuthenticationMessage(authType, authToken); //RPC ID?
+				.WithRequestID() //RPC ID?
+				.WithAuthenticationMessage(authType, authToken); 
 
 			//Now we add the requests to the envelope
 			foreach (Request r in requestMessages)
@@ -31,29 +32,18 @@ namespace PokemonGo.RocketAPI.Helpers
 			return envelope;
         }
 
-        public static RequestEnvelope GetInitialRequestEnvelope(string authToken, AuthType authType, double lat, double lng,
-            double altitude, params RequestType[] customRequestTypes)
-        {
-            var customRequests = customRequestTypes.ToList().Select(c => new Request.Types.Requests {Type = (int) c});
-            return GetInitialRequest(authToken, authType, lat, lng, altitude, customRequests.ToArray());
-        }
-
         public static RequestEnvelope GetRequestEnvelope(AuthTicket authTicket, double lat, double lng, double altitude)
         {
-            return new Request
-            {
-                Altitude = Utils.FloatAsUlong(altitude),
-                authTicket = authTicket,
-                Latitude = Utils.FloatAsUlong(lat),
-                Longitude = Utils.FloatAsUlong(lng),
-                RpcId = 1469378659230941192,
-                Unknown1 = 2,
-                Unknown12 = 989, //Required otherwise we receive incompatible protocol
-                Requests =
-                {
-                    customRequests
-                }
-            };
+			RequestEnvelope envelope = new RequestEnvelope();
+
+			//These requests are sent with our issued AuthTicket
+			envelope.WithAltitude(altitude)
+				.WithLatitude(lat)
+				.WithLongitude(lng)
+				.WithRequestID() //RPC ID?
+				.WithAuthTicket(authTicket);
+
+			return envelope;
         }
     }
 }
