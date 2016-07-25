22786ce0324d75716c63a9b162ffa13c7d71edd5
Maxime GRIOT <yamashi@live.com>
2016-07-25 17:58:28 +0200

    Added missing file

---
+++ PokemonGo.RocketAPI.Logic/Utils/JitterUtils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonGo.RocketAPI.Logic.Utils
+{
+    public static class JitterUtils
+    {
+        private static Random _randomDevice = new Random();
+
+        public static Task RandomDelay(int min, int max)
+        {
+            return Task.Delay(_randomDevice.Next(min, max));
+        }
+    }
+}
