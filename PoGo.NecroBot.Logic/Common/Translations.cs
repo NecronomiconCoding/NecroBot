c6d2658eb35a83329de6be536e7a721d7de33bf1
Pakobbix <Pakobbix@gmail.com>
2016-07-27 23:54:06 +0200

    fixed: Commata for "Incubated egg has hatched"

--- PoGo.NecroBot.Logic/Common/Translations.cs
+++ PoGo.NecroBot.Logic/Common/Translations.cs
@@ -69,7 +69,7 @@ namespace PoGo.NecroBot.Logic.Common
             new KeyValuePair<TranslationString, string>(TranslationString.IncubatorStatusUpdate,
                 "Incubator status update: {0:0.00}km left"),
             new KeyValuePair<TranslationString, string>(TranslationString.IncubatorEggHatched,
-                "Incubated egg has hatched: {0}")
+                "Incubated egg has hatched: {0}"),
             new KeyValuePair<TranslationString, string>(Common.TranslationString.LogEntryError, "ERROR"),
             new KeyValuePair<TranslationString, string>(Common.TranslationString.LogEntryAttention, "ATTENTION"),
             new KeyValuePair<TranslationString, string>(Common.TranslationString.LogEntryInfo, "INFO"),
