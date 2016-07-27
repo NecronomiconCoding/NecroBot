3c413c2a6a0f9df042ffce458d0d06467e9c6a0e
Maxime GRIOT <yamashi@live.com>
2016-07-27 23:58:32 +0200

    Started to move towards OOP

---
+++ PoGo.NecroBot.Logic/Tasks/Login.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoGo.NecroBot.Logic.Tasks
+{
+    public interface ILogin
+    {
+        void DoLogin();
+    }
+    public class Login : ILogin
+    {
+        private readonly PokemonGo.RocketAPI.Rpc.Login _loginApi;
+        public Login(PokemonGo.RocketAPI.Rpc.Login loginApi)
+        {
+            _loginApi = loginApi;
+        }
+
+        public void DoLogin()
+        {
+
+        }
+    }
+}
