01d9b07a80299904d1bdbd0fd5be239965e76187
Marcin Bobowski <marcin@citystudio.pl>
2016-07-28 07:11:55 +0200

    WIP: randomized delay

--- PoGo.NecroBot.Logic/Utils/DelayingUtils.cs
+++ PoGo.NecroBot.Logic/Utils/DelayingUtils.cs
@@ -6,16 +6,23 @@ namespace PoGo.NecroBot.Logic.Utils
 {
     public static class DelayingUtils
     {
+        private static readonly Random RandomDevice = new Random();
+
         public static void Delay(int delay, int defdelay)
         {
             if (delay > 0)
             {
-                Logger.Write($"Waiting (inside of DelayingUtils) for {delay / 1000} seconds using global delay");
-                Thread.Sleep(delay);
+                float randomFactor = 0.3f;
+                int randomMin = (int) delay * (1 - randomFactor);
+                int randomMax = (int) delay * (1 + randomFactor);
+                int randomizedDelay = RandomDevice.Next(randomMin, randomMax);
+               
+                Logger.Write($"Waiting (inside of DelayingUtils) for {randomizedDelay / 1000:0.#} seconds using global delay");
+                Thread.Sleep(randomizedDelay);
             }
             else if (defdelay > 0)
             {
-                Logger.Write($"Waiting (inside of DelayingUtils) for {defdelay / 1000} seconds using default delay");
+                Logger.Write($"Waiting (inside of DelayingUtils) for {defdelay / 1000:0.#} seconds using default delay");
                 Thread.Sleep(defdelay);
             }
         }
