7af715bcf93bf5f171495e51fa8aff542f151d5a
dually8 <coffey.charlesj@gmail.com>
2016-07-28 00:10:13 -0400

    Added program pause before continuing with farming.

---
+++ PoGo.NecroBot.Logic/State/PauseState.cs
@@ -0,0 +1,36 @@
+﻿#region using directives
+
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using PoGo.NecroBot.Logic.Common;
+using PoGo.NecroBot.Logic.Event;
+using PoGo.NecroBot.Logic.Utils;
+using PoGo.NecroBot.Logic.Tasks;
+
+#endregion
+
+namespace PoGo.NecroBot.Logic.State
+{
+    public class PauseState : IState
+    {
+        public async Task<IState> Execute(ISession session)
+        {
+
+            // Pauses so user has a chance to exit program if location is incorrect
+            // this will help if/when the config/auth.json files are not present
+            // and are being re-created.
+
+            session.EventDispatcher.Send(new WarnEvent
+            {
+                Message = "Press any key twice to continue or press 'q' to quit."
+            });
+            // I honestly don't know why it's twice.  
+            // Pressing it once does nothing most of the time.
+
+            PauseTask.Execute(session).Wait();
+            
+            return new InfoState();
+        }
+    }
+}
---
+++ PoGo.NecroBot.Logic/Tasks/PauseTask.cs
@@ -0,0 +1,27 @@
+﻿#region using directives
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using PoGo.NecroBot.Logic.Event;
+using PoGo.NecroBot.Logic.State;
+using PokemonGo.RocketAPI;
+using POGOProtos.Inventory.Item;
+
+#endregion
+
+namespace PoGo.NecroBot.Logic.Tasks
+{
+    public class PauseTask
+    {
+        public static async Task Execute(ISession session)
+        {
+            ConsoleKeyInfo str;
+            do
+            {
+                str = Console.ReadKey(true);
+            } while (str == null);
+            Console.WriteLine("Got key");
+        }
+    }
+}
