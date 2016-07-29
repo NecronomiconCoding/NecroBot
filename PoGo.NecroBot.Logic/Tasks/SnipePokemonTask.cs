1a0a192d637db90707506e984be59f80ebb950aa
5andr0 <sandro.trianni@gmail.com>
2016-07-29 14:44:30 +0200

    Revert "Fixed comma lat long"
    This reverts commit 606a2075d2bdfda0d08e2b01fe2828744c08c5af.

--- PoGo.NecroBot.Logic/Tasks/SnipePokemonTask.cs
+++ PoGo.NecroBot.Logic/Tasks/SnipePokemonTask.cs
@@ -54,7 +54,6 @@ namespace PoGo.NecroBot.Logic.Tasks
 
         public bool Equals(PokemonLocation obj)
         {
-            Logging.Logger.Write($"Equals check: {latitude}= {obj.latitude} && {longitude} ={obj.longitude}");
             return latitude == obj.latitude && longitude == obj.longitude;
         }
     }
@@ -94,12 +93,9 @@ namespace PoGo.NecroBot.Logic.Tasks
                         var info = JsonConvert.DeserializeObject<SniperInfo>(line);
 
                         if (snipeLocations.Any(x => 
-                                Math.Abs(x.latitude - info.latitude) < 0.001 && 
-                                Math.Abs(x.longitude - info.longitude) < 0.001)) // we might have different precisions from other sources
-                        {
-                            Logging.Logger.Write($"Equal coords:  {info.latitude} && {info.longitude}");
+                                Math.Abs(x.latitude - info.latitude) < 0.0001 && 
+                                Math.Abs(x.longitude - info.longitude) < 0.0001)) // we might have different precisions from other sources
                             continue;
-                        }
 
                         snipeLocations.RemoveAll(x => DateTime.Now > x.timeStampAdded.AddMinutes(15));
                         snipeLocations.Add(info);
