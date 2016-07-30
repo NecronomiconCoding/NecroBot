5573b361ff230e4cbd77512e8b59b127b3e37bde
BadWolfL <hsyllzd@gmail.com>
2016-07-31 00:15:31 +0200

    Impltement optimized recycling balls
    If the amount of total balls currently hold is greater than
    TotalAmountOfPokebalsToKeep, the Bot will recycle poke balls from low
    level balls.
    
    In Configs, TotalAmountOfPokebalsToKeep should be less than the sum of
    all separate poke balls in ItemRecycleFilter.

--- PoGo.NecroBot.Logic/Tasks/RecycleItemsTask.cs
+++ PoGo.NecroBot.Logic/Tasks/RecycleItemsTask.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using PoGo.NecroBot.Logic.Event;
 using PoGo.NecroBot.Logic.State;
 using PoGo.NecroBot.Logic.Utils;
+using POGOProtos.Inventory.Item;
 
 #endregion
 
@@ -28,6 +29,89 @@ namespace PoGo.NecroBot.Logic.Tasks
 
                 DelayingUtils.Delay(session.LogicSettings.DelayBetweenPlayerActions, 500);
             }
+
+            var pokeBallsCount = await session.Inventory.GetItemAmountByType(ItemId.ItemPokeBall);
+            var greatBallsCount = await session.Inventory.GetItemAmountByType(ItemId.ItemGreatBall);
+            var ultraBallsCount = await session.Inventory.GetItemAmountByType(ItemId.ItemUltraBall);
+            var masterBallsCount = await session.Inventory.GetItemAmountByType(ItemId.ItemMasterBall);
+
+            int pokeBallsToRecycle = 0;
+            int greatBallsToRecycle = 0;
+            int ultraBallsToRecycle = 0;
+            int masterBallsToRecycle = 0;
+
+            int totalBallsCount = pokeBallsCount + greatBallsCount + ultraBallsCount + masterBallsCount;
+            if(totalBallsCount>session.LogicSettings.TotalAmountOfPokebalsToKeep)
+            {
+                int diff = totalBallsCount - session.LogicSettings.TotalAmountOfPokebalsToKeep;
+                if (diff > 0)
+                {
+                    int pokeBallsToKeep = pokeBallsCount - diff;
+                    if (pokeBallsToKeep < 0)
+                    {
+                        pokeBallsToKeep = 0;
+                    }
+                    pokeBallsToRecycle = pokeBallsCount - pokeBallsToKeep;
+                    diff -= pokeBallsToRecycle;
+
+                    cancellationToken.ThrowIfCancellationRequested();
+                    await session.Client.Inventory.RecycleItem(ItemId.ItemPokeBall, pokeBallsToRecycle);
+                    session.EventDispatcher.Send(new ItemRecycledEvent { Id = ItemId.ItemPokeBall, Count = pokeBallsToRecycle });
+                    DelayingUtils.Delay(session.LogicSettings.DelayBetweenPlayerActions, 500);
+                }
+
+                if (diff>0)
+                {
+                    int greatBallsToKeep = greatBallsCount - diff;
+                    if (greatBallsToKeep < 0)
+                    {
+                        greatBallsToKeep = 0;
+                    }
+                    greatBallsToRecycle = greatBallsCount - greatBallsToKeep;
+                    diff -= greatBallsToRecycle;
+                    cancellationToken.ThrowIfCancellationRequested();
+                    await session.Client.Inventory.RecycleItem(ItemId.ItemGreatBall, greatBallsToRecycle);
+                    session.EventDispatcher.Send(new ItemRecycledEvent { Id = ItemId.ItemGreatBall, Count = greatBallsToRecycle });
+                    DelayingUtils.Delay(session.LogicSettings.DelayBetweenPlayerActions, 500);
+                }
+
+                // Don't Recycle Ultra Balls
+                /*
+                if (diff > 0)
+                {
+                    int ultraBallsToKeep = ultraBallsCount - diff;
+                    if (ultraBallsToKeep < 0)
+                    {
+                        ultraBallsToKeep = 0;
+                    }
+                    ultraBallsToRecycle = ultraBallsCount - ultraBallsToKeep;
+                    diff -= ultraBallsToRecycle;
+                    cancellationToken.ThrowIfCancellationRequested();
+                    await session.Client.Inventory.RecycleItem(ItemId.ItemUltraBall, ultraBallsToRecycle);
+                    session.EventDispatcher.Send(new ItemRecycledEvent { Id = ItemId.ItemUltraBall, Count = ultraBallsToRecycle });
+                    DelayingUtils.Delay(session.LogicSettings.DelayBetweenPlayerActions, 500);
+                }
+                */
+
+                // No Master Balls in Game, so far
+                /*
+                if (diff > 0)
+                {
+                    int masterBallsToKeep = masterBallsCount - diff;
+                    if (masterBallsToKeep < 0)
+                    {
+                        masterBallsToKeep = 0;
+                    }
+                    masterBallsToRecycle = masterBallsCount - masterBallsToKeep;
+                    diff -= masterBallsToRecycle;
+                    cancellationToken.ThrowIfCancellationRequested();
+                    await session.Client.Inventory.RecycleItem(ItemId.ItemMasterBall, masterBallsToRecycle);
+                    session.EventDispatcher.Send(new ItemRecycledEvent { Id = ItemId.ItemMasterBall, Count = masterBallsToRecycle });
+                    DelayingUtils.Delay(session.LogicSettings.DelayBetweenPlayerActions, 500);
+                }
+                */   
+            }
+
 
             await session.Inventory.RefreshCachedInventory();
         }
