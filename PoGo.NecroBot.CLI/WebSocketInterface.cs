9f5928417b9b8148f26bf8e76ecf4ec7628db570
Nicolas Schmitt <nicolas.schmitt@outlook.fr>
2016-07-30 19:58:44 +0200

    Send coordinate to new clients

--- PoGo.NecroBot.CLI/WebSocketInterface.cs
+++ PoGo.NecroBot.CLI/WebSocketInterface.cs
@@ -97,6 +97,16 @@ namespace PoGo.NecroBot.CLI
 
             if (_lastPokeStopList != null)
                 session.Send(Serialize(_lastPokeStopList));
+
+            try
+            {
+                session.Send(Serialize(new UpdatePositionEvent()
+                {
+                    Latitude = _session.Client.CurrentLatitude,
+                    Longitude = _session.Client.CurrentLongitude
+                }));
+            } catch { }
+
         }
 
         public void Listen(IEvent evt, Session session)
