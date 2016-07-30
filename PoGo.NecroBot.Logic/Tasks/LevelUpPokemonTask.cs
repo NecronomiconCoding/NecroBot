66fa56e82bf03ed3fa7dfe2c66030b86d4191987
Kenny Brown <kenny@pwnlog.com>
2016-07-30 13:28:58 -0500

    Upgrade all pokemon above IV %
    The previous code was all sorts of broken for and even when I did finally get it to work it only upgraded like 1 pokemon once... so I looped it and made it get all pokemon above IV instead of just from your top list

--- PoGo.NecroBot.Logic/Tasks/LevelUpPokemonTask.cs
+++ PoGo.NecroBot.Logic/Tasks/LevelUpPokemonTask.cs
@@ -14,31 +14,29 @@ namespace PoGo.NecroBot.Logic.Tasks
     {
         public static async Task Execute(ISession session, CancellationToken cancellationToken)
         {
-            if (DisplayPokemonStatsTask.PokemonId.Count == 0 || DisplayPokemonStatsTask.PokemonIdcp.Count == 0)
-            {
-                return;
-            }
             if (session.LogicSettings.LevelUpByCPorIv.ToLower().Contains("iv"))
             {
-                var rand = new Random();
-                var randomNumber = rand.Next(0, DisplayPokemonStatsTask.PokemonId.Count - 1);
-
-                var upgradeResult =
-                    await session.Inventory.UpgradePokemon(DisplayPokemonStatsTask.PokemonId[randomNumber]);
-                if (upgradeResult.Result.ToString().ToLower().Contains("success"))
+                foreach (var pokeId in session.Inventory.GetPokemons().Result.Where(pokemonData => PokemonInfo.CalculatePokemonPerfection(pokemonData) >= session.LogicSettings.KeepMinIvPercentage).Select(p => p.Id))
                 {
-                    Logger.Write("Pokemon Upgraded:" + upgradeResult.UpgradedPokemon.PokemonId + ":" +
-                                 upgradeResult.UpgradedPokemon.Cp);
-                }
-                else if (upgradeResult.Result.ToString().ToLower().Contains("insufficient"))
-                {
-                    Logger.Write("Pokemon Upgrade Failed Not Enough Resources");
-                }
-                else
-                {
-                    Logger.Write(
-                        "Pokemon Upgrade Failed Unknown Error, Pokemon Could Be Max Level For Your Level The Pokemon That Caused Issue Was:" +
-                        upgradeResult.UpgradedPokemon.PokemonId);
+                    while (true)
+                    {
+                        var upgradeResult = await session.Inventory.UpgradePokemon(pokeId);
+                        if (upgradeResult.Result.ToString().ToLower().Contains("success"))
+                        {
+                            Logger.Write("Pokemon Upgraded:" + upgradeResult.UpgradedPokemon.PokemonId + ":" +
+                                         upgradeResult.UpgradedPokemon.Cp);
+                        }
+                        else if (upgradeResult.Result.ToString().ToLower().Contains("insufficient"))
+                        {
+                            Logger.Write("Pokemon Upgrade Failed Not Enough Resources");
+                            break;
+                        }
+                        else
+                        {
+//                            Logger.Write("Pokemon Upgrade Failed Unknown Error, Pokemon Could Be Max Level For Your Level The Pokemon That Caused Issue Was:" + upgradeResult.UpgradedPokemon.PokemonId);
+                            break;
+                        }
+                    }
                 }
             }
             else if (session.LogicSettings.LevelUpByCPorIv.ToLower().Contains("cp"))
