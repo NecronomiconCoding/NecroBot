78f4e8d992e7bf0755e7a8bbd343230ca886bba3
TheTravelingTrainer <chappie4861@gmail.com>
2016-07-31 00:58:48 +0100

    Updates to websockets to handle command based communcation (#1497)
    * Added inital command interface for web sockets
    
    * added back in legacy calls
    
    * didnt save all files
    
    * async issues

---
+++ PoGo.NecroBot.CLI/WebSocketHandler/IWebSocketResponce.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoGo.NecroBot.CLI.WebSocketHandler
+{
+    interface IWebSocketResponce
+    {
+        string Command { get; }
+        dynamic Data { get; }
+    }
+}
