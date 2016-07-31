e88376ddf8a736e2c1a456870c7d37beca9c9a11
TheTravelingTrainer <chappie4861@gmail.com>
2016-07-31 02:37:38 +0100

    egg list web request

---
+++ PoGo.NecroBot.CLI/WebSocketHandler/BasicGetCommands/Events/EggListResponce.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoGo.NecroBot.CLI.WebSocketHandler.BasicGetCommands.Events
+{
+    public class EggListResponce : IWebSocketResponce
+    {
+        public EggListResponce(dynamic data)
+        {
+            Command = "EggListWeb";
+            Data = data;
+        }
+        public string Command { get; private set; }
+        public dynamic Data { get; private set; }
+    }
+}
