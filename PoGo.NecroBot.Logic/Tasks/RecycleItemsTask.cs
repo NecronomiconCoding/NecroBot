d39504d6ab1382182a397c63b65d25435e4d47d3
NecronomiconCoding <tvgfx123@gmail.com>
2016-07-31 19:30:47 +0200

    code improvements

--- PoGo.NecroBot.Logic/Tasks/RecycleItemsTask.cs
+++ PoGo.NecroBot.Logic/Tasks/RecycleItemsTask.cs
@@ -68,8 +68,6 @@ namespace PoGo.NecroBot.Logic.Tasks
             int pokeBallsToRecycle = 0;
             int greatBallsToRecycle = 0;
             /*
-            int ultraBallsToRecycle = 0;
-            int masterBallsToRecycle = 0;
             */
 
             int totalBallsCount = pokeBallsCount + greatBallsCount + ultraBallsCount + masterBallsCount;
