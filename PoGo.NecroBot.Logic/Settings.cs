259aed7b3e5c66f8d89ea0b45de0654f7e93f96b
TheTravelingTrainer <chappie4861@gmail.com>
2016-07-31 14:26:37 +0100

    had wrong ver

--- PoGo.NecroBot.Logic/Settings.cs
+++ PoGo.NecroBot.Logic/Settings.cs
@@ -103,7 +103,7 @@ namespace PoGo.NecroBot.Logic
 
         public bool AutomaticallyLevelUpPokemon = false;
 
-        public bool AutoUpdate = false;
+        public bool AutoUpdate = true;
         public double DefaultAltitude = 10;
         public double DefaultLatitude = 40.785091;
         public double DefaultLongitude = -73.968285;
