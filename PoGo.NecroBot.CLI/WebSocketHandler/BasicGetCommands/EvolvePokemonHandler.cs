7e09c0855b661573c6d7c7e4ccd8880124921a97
Nicolas Schmitt <nicolas.schmitt@outlook.fr>
2016-08-01 23:01:58 +0200

    fix

--- PoGo.NecroBot.CLI/WebSocketHandler/BasicGetCommands/EvolvePokemonHandler.cs
+++ PoGo.NecroBot.CLI/WebSocketHandler/BasicGetCommands/EvolvePokemonHandler.cs
@@ -9,14 +9,18 @@ using PoGo.NecroBot.Logic.State;
 
 namespace PoGo.NecroBot.CLI.WebSocketHandler.BasicGetCommands
 {
+    public class EvolvePokemonHandler : IWebSocketRequestHandler
     {
         public string Command { get; private set;}
 
+        public EvolvePokemonHandler()
         {
+            Command = "EvolvePokemon";
         }
 
         public async Task Handle(ISession session, WebSocketSession webSocketSession, dynamic message)
         {
+            await EvolvePokemonTask.Execute(session, webSocketSession, (ulong)message.PokemonId, (string)message.RequestID);
         }
     }
 }
