dba1b85cba6ddbd3cbd87d9bcdb4c0e131a663d8
Nicolas Schmitt <nicolas.schmitt@outlook.fr>
2016-08-02 00:34:00 +0200

    Add correct delay

--- PoGo.NecroBot.Logic/Tasks/EggsListTask.cs
+++ PoGo.NecroBot.Logic/Tasks/EggsListTask.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using PoGo.NecroBot.Logic.Event;
 using PoGo.NecroBot.Logic.State;
 using POGOProtos.Inventory.Item;
+using PoGo.NecroBot.Logic.Utils;
 
 #endregion
 
@@ -14,6 +15,7 @@ namespace PoGo.NecroBot.Logic.Tasks
     {
         public static async Task Execute(ISession session)
         {
+            // Refresh inventory so that the player stats are fresh
             await session.Inventory.RefreshCachedInventory();
 
             var playerStats = (await session.Inventory.GetPlayerStats()).FirstOrDefault();
@@ -39,6 +41,8 @@ namespace PoGo.NecroBot.Logic.Tasks
                     Incubators = incubators,
                     UnusedEggs = unusedEggs
                 });
+
+            DelayingUtils.Delay(session.LogicSettings.DelayBetweenPlayerActions, 0);
         }
     }
 }
