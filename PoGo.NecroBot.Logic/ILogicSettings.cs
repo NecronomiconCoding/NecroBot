6c109d643e8d403393d6f567b421a2b63ca9fd5c
simplyphp <ciamos@gmail.com>
2016-08-02 10:52:46 -0500

    Made PokemonsTransferFilter accept KeepMinOperator on an individual Pokemon basis

--- PoGo.NecroBot.Logic/ILogicSettings.cs
+++ PoGo.NecroBot.Logic/ILogicSettings.cs
@@ -46,12 +46,13 @@ namespace PoGo.NecroBot.Logic
         {
         }
 
-        public TransferFilter(int keepMinCp, float keepMinIvPercentage, int keepMinDuplicatePokemon, 
+        public TransferFilter(int keepMinCp, float keepMinIvPercentage, string keepMinOperator, int keepMinDuplicatePokemon, 
             List<PokemonMove> moves = null)
         {
             KeepMinCp = keepMinCp;
             KeepMinIvPercentage = keepMinIvPercentage;
             KeepMinDuplicatePokemon = keepMinDuplicatePokemon;
+            KeepMinOperator = keepMinOperator;
             Moves = moves ?? new List<PokemonMove>();
         }
 
@@ -59,6 +60,7 @@ namespace PoGo.NecroBot.Logic
         public float KeepMinIvPercentage { get; set; }
         public int KeepMinDuplicatePokemon { get; set; }
         public List<PokemonMove> Moves { get; set; }
+        public string KeepMinOperator { get; set; }
     }
 
     public interface ILogicSettings
