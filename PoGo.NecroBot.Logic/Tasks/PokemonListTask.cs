bbf20dae8ac66dc8f098ff135d59cf923cac552a
Nicolas Schmitt <nicolas.schmitt@outlook.fr>
2016-08-02 09:23:51 +0200

    Event: Add candy to pokemon list

--- PoGo.NecroBot.Logic/Tasks/PokemonListTask.cs
+++ PoGo.NecroBot.Logic/Tasks/PokemonListTask.cs
@@ -15,14 +15,32 @@ namespace PoGo.NecroBot.Logic.Tasks
     {
         public static async Task Execute(ISession session)
         {
+            // Refresh inventory so that the player stats are fresh
+            await session.Inventory.RefreshCachedInventory();
+
+            var myPokemonSettings = await session.Inventory.GetPokemonSettings();
+            var pokemonSettings = myPokemonSettings.ToList();
+
+            var myPokemonFamilies = await session.Inventory.GetPokemonFamilies();
+            var pokemonFamilies = myPokemonFamilies.ToArray();
+
             var allPokemonInBag = await session.Inventory.GetHighestsCp(1000);
-            var pkmWithIv = allPokemonInBag.Select(p => Tuple.Create(p, PokemonInfo.CalculatePokemonPerfection(p)));
-            session.EventDispatcher.Send(
-                new PokemonListEvent
-                {
-                    PokemonList = pkmWithIv.ToList()
-                });
-            await Task.Delay(500);
+
+            var pkmWithIv = allPokemonInBag.Select(p => {
+                var settings = pokemonSettings.Single(x => x.PokemonId == p.PokemonId);
+                return Tuple.Create(
+                    p,
+                    PokemonInfo.CalculatePokemonPerfection(p),
+                    pokemonFamilies.Single(x => settings.FamilyId == x.FamilyId).Candy_
+                );
+            });
+
+            session.EventDispatcher.Send(new PokemonListEvent
+            {
+                PokemonList = pkmWithIv.ToList()
+            });
+
+            await Task.Delay(session.LogicSettings.DelayBetweenPlayerActions);
         }
     }
 }
