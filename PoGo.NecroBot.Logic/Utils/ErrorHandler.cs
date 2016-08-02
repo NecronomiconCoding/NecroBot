990f7a78212b36313ab7caf0d0ac22f4b14d7cdf
Andrerm124 <andrerm124@gmail.com>
2016-08-02 13:49:58 +0100

    Added Translation Serialization Error Handling
    Changed so that if an error occurs in the loading of a translation file.
    
    This often occurred if a type was made in the Key of a file. Currently
    it can only resolve issues with the EN file however will display issues
    with alternate languages.
    
    I've also created a ThrowFatalError method which I quite like.

---
+++ PoGo.NecroBot.Logic/Utils/ErrorHandler.cs
@@ -0,0 +1,35 @@
+﻿using PoGo.NecroBot.Logic.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoGo.NecroBot.Logic.Utils
+{
+    class ErrorHandler
+    {
+        /// <summary>
+        /// Alerts that a fatal error has occurred, displaying a message and exiting the application
+        /// </summary>
+        /// <param name="strMessage">Optional message to display - Leave NULL to exclude message</param>
+        /// <param name="timeout">The total seconds the messag will display before shutting down</param>
+        public static void ThrowFatalError( string strMessage, int timeout )
+        {
+            Logger.Write( "[ERROR] Fatal Error", LogLevel.Error );
+
+            if( strMessage != null )
+                Console.WriteLine( strMessage );
+
+            Console.Write( "Ending Application... " );
+
+            for( int i = timeout; i > 0; i-- )
+            {
+                Console.Write( "\b" + i );
+                System.Threading.Thread.Sleep( 1000 );
+            }
+
+            Environment.Exit( -1 );
+        }
+    }
+}
