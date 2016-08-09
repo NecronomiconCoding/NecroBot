46c32079d0848734d45178a97e8c74bea8334192
nicolas.haefelin <nicolas.haefelin@MW792O7J5V4I8V>
2016-08-09 20:02:12 +0200

    Added minEggKm for limited incubators to avoid using limited incubators on anything but 10km eggs (settings can be changed to use also 5km eggs or all eggs)

--- PoGo.NecroBot.Logic/Settings.cs
+++ PoGo.NecroBot.Logic/Settings.cs
@@ -351,6 +351,8 @@ namespace PoGo.NecroBot.Logic
         //lucky, incense and berries
         [DefaultValue(true)]
         public bool UseEggIncubators;
+        [DefaultValue(10)]
+        public int minEggKmForLimitedIncubators;
         [DefaultValue(false)]
         public bool UseLuckyEggConstantly;
         [DefaultValue(30)]
@@ -1249,6 +1251,7 @@ namespace PoGo.NecroBot.Logic
         public bool TransferDuplicatePokemon => _settings.TransferDuplicatePokemon;
         public bool TransferDuplicatePokemonOnCapture => _settings.TransferDuplicatePokemonOnCapture;
         public bool UseEggIncubators => _settings.UseEggIncubators;
+        public int minEggKmForLimitedIncubators => _settings.minEggKmForLimitedIncubators;
         public int UseGreatBallAboveCp => _settings.UseGreatBallAboveCp;
         public int UseUltraBallAboveCp => _settings.UseUltraBallAboveCp;
         public int UseMasterBallAboveCp => _settings.UseMasterBallAboveCp;
