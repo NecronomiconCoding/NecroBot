78d3a89d3636da3f2c6155cc15971b51b69ab693
grimmgringo <grimmgringo@hmamail.com>
2016-08-10 03:15:54 -0500

    Remainder of plugin removal

--- PoGo.NecroBot.Logic/Properties/AssemblyInfo.cs
+++ PoGo.NecroBot.Logic/Properties/AssemblyInfo.cs
@@ -39,5 +39,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.8.4")]
+[assembly: AssemblyVersion("0.8.5")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
