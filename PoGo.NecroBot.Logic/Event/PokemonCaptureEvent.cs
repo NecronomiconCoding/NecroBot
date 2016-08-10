3c495ed255d4e6a26d5550e0ccb9e149a03dae2a
firefoxpdm <firefoxpdm@users.noreply.github.com>
2016-08-10 21:07:30 +0200

    Field starts with caps

--- PoGo.NecroBot.Logic/Event/PokemonCaptureEvent.cs
+++ PoGo.NecroBot.Logic/Event/PokemonCaptureEvent.cs
@@ -18,7 +18,7 @@ namespace PoGo.NecroBot.Logic.Event
         public int Exp;
         public int FamilyCandies;
         public PokemonId Id;
-        public ulong uniqueId;
+        public ulong UniqueId;
         public double Level;
         public int MaxCp;
         public double Perfection;
