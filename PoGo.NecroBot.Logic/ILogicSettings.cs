7da0ee79fe99e5d2380023cea9b5e7b423b34382
KOTR2003 <KOTR2003@users.noreply.github.com>
2016-08-11 06:54:55 -0400

    Update ILogicSettings.cs

--- PoGo.NecroBot.Logic/ILogicSettings.cs
+++ PoGo.NecroBot.Logic/ILogicSettings.cs
@@ -98,6 +98,8 @@ namespace PoGo.NecroBot.Logic
         double UseMasterBallBelowCatchProbability { get; }
         double UseUltraBallBelowCatchProbability { get; }
         double UseGreatBallBelowCatchProbability { get; }
+        bool EnableMissedThrows { get; }
+        int ThrowMissPercentage { get; }
         bool EnableHumanizedThrows { get; }
         int NiceThrowChance { get; }
         int GreatThrowChance { get; }
