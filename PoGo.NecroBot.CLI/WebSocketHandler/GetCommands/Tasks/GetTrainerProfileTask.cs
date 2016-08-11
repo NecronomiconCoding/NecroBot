ffd53d22adbe61841d3951b2176fce41f92e9bef
ChemiHatake <kuroichemi@gmail.com>
2016-08-11 07:03:35 +0300

    Code Cleanup

--- PoGo.NecroBot.CLI/WebSocketHandler/GetCommands/Tasks/GetTrainerProfileTask.cs
+++ PoGo.NecroBot.CLI/WebSocketHandler/GetCommands/Tasks/GetTrainerProfileTask.cs
@@ -1,13 +1,13 @@
-﻿using System.Linq;
-using System.Threading.Tasks;
-using PoGo.NecroBot.CLI.WebSocketHandler.GetCommands.Events;
+﻿using PoGo.NecroBot.CLI.WebSocketHandler.GetCommands.Events;
 using PoGo.NecroBot.CLI.WebSocketHandler.GetCommands.Helpers;
 using PoGo.NecroBot.Logic.State;
 using SuperSocket.WebSocket;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace PoGo.NecroBot.CLI.WebSocketHandler.GetCommands.Tasks
 {
-    class GetTrainerProfileTask
+    internal class GetTrainerProfileTask
     {
         public static async Task Execute(ISession session, WebSocketSession webSocketSession, string requestID)
         {
