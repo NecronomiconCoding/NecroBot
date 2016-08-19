07fb82a6b21a92733ef71ab437965dd270f2e418
NoxxDev <noxx.mx@gmail.com>
2016-08-19 11:33:08 -0300

    Update assembly version

--- PoGo.NecroBot.CLI/Properties/AssemblyInfo.cs
+++ PoGo.NecroBot.CLI/Properties/AssemblyInfo.cs
@@ -39,5 +39,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.9.2")]
-[assembly: AssemblyFileVersion("0.9.2.0")]
+[assembly: AssemblyVersion("0.9.3")]
+[assembly: AssemblyFileVersion("0.9.3.0")]
