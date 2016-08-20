bc12cb77c6ab0c682f897685618b609a049ebeb5
NoxxDev <noxx.mx@gmail.com>
2016-08-20 14:44:20 -0300

    Change killswitch settings

---
+++ PoGo.NecroBot.Logic/Model/Settings/KillSwitchSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoGo.NecroBot.Logic.Model.Settings
+{
+    public class KillSwitchSettings
+    {
+        public bool UseKillSwitchCatch = false;
+        public int CatchErrorPerHours = 40;
+        public int CatchEscapePerHours = 40;
+        public int CatchFleePerHours = 40;
+        public int CatchMissedPerHours = 40;
+        public int CatchSuccessPerHours = 40;
+        public bool UseKillSwitchPokestops = false;
+        public int AmountPokestops = 80;
+    }
+}
