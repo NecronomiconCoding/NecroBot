c2510afc60213f59a9dfeecbd58cc212a96e5f03
Truong Nguyen <samuraitruong@hotmail.com>
2016-08-27 17:14:27 +1000

    cleanup before merging with master

---
+++ PoGo.NecroBot.CLI/WebSocketHandler/ActionCommands/HumanSnipeRemovePokemonHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using PoGo.NecroBot.Logic.State;
+using SuperSocket.WebSocket;
+using POGOProtos.Inventory.Item;
+
+namespace PoGo.NecroBot.CLI.WebSocketHandler.ActionCommands
+{
+    public class HumanSnipeRemovePokemonHandler : IWebSocketRequestHandler
+    {
+        public string Command { get; private set;}
+
+        public HumanSnipeRemovePokemonHandler()
+        {
+            Command = "RemovePokemon";
+        }
+
+        public async Task Handle(ISession session, WebSocketSession webSocketSession, dynamic message)
+        {
+            await Logic.Tasks.HumanWalkSnipeTask.RemovePokemonFromQueue(session, (string)message.Id);
+            await Task.Delay(1000);// Logic.Tasks.RecycleItemsTask.DropItem(session, (ItemId)message.ItemId, (int)message.Count);
+        }
+    }
+}
