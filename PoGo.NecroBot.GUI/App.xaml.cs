72c17553f09469dd7e9eb942e9b0cc9fe5512e64
Sébastien [NzV] <donm7v@gmail.com>
2016-09-06 02:31:42 +0200

    Add WebUi Client Manager GUI
    Allow to install/update/auto-update/unsinstall easily webclient ui.

--- PoGo.NecroBot.GUI/App.xaml.cs
+++ PoGo.NecroBot.GUI/App.xaml.cs
@@ -1,11 +1,9 @@
 ﻿#region using directives
 
 using System;
-using System.IO;
 using System.Reflection;
 using System.Windows;
 using Awesomium.Core;
-using PoGo.NecroBot.GUI.WebUiClient;
 
 #endregion
 
@@ -18,16 +16,6 @@ namespace PoGo.NecroBot.GUI
     {
         protected override void OnStartup(StartupEventArgs e)
         {
-            var settings = new WebUiClientConfig();
-            var settingsPath = Path.Combine(Directory.GetCurrentDirectory(),
-                "Config" + Path.DirectorySeparatorChar + "WebUiClient.json");
-            settings.Load(settingsPath);
-            var webUi = settings.WebUiClients[settings.CurrentWebUiClient];
-            if (!webUi.IsUpToDate())
-                webUi.DonwloadAndInstall();
-            if (!webUi.IsInstalled())
-                return;
-
             // Initialization must be performed here,
             // before creating a WebControl.
             if (!WebCore.IsInitialized)
@@ -35,7 +23,7 @@ namespace PoGo.NecroBot.GUI
                 var baseUri = new Uri(Assembly.GetEntryAssembly().Location);
                 WebCore.Initialize(new WebConfig
                 {
-                    HomeURL = new Uri(baseUri, webUi.HomeUri),
+                    HomeURL = new Uri(baseUri, "WebUi/"),
                     LogPath = @".\starter.log",
                     LogLevel = LogLevel.Verbose,
                     RemoteDebuggingPort = 9033
